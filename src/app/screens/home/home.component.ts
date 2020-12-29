b6b77aacb059f1e786906be92187891351752bfb
Bruno <bruno@Mac-mini-de-Bruno.local>
2020-12-29 17:19:56 -0300

    change sass to css, change background

--- src/app/screens/home/home.component.ts
+++ src/app/screens/home/home.component.ts
@@ -4,7 +4,7 @@ import { PokemonService } from 'src/app/services/pokemon.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.sass']
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
