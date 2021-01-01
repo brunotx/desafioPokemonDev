04c0ce2c0ab14fa4c3c4debe31a17bf20a5080f8
Bruno <bruno@Mac-mini-de-Bruno.local>
2021-01-01 12:27:20 -0300

    shearch ok

--- src/app/screens/home/home.component.ts
+++ src/app/screens/home/home.component.ts
@@ -1,34 +1,50 @@
-import { Component, OnInit, TemplateRef } from '@angular/core';
+import { Component, OnDestroy, OnInit, TemplateRef } from '@angular/core';
 import { PokemonService } from 'src/app/services/pokemon.service';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 import { PokemonComponent } from '../pokemon/pokemon.component';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
   pokemonList = [];
   nextPage: string;
   modalRef: BsModalRef;
+  findPokemon: string;
+  notFound: boolean = false;
+  errorMessage: string;
+  loading: boolean = false;
+  unsubscribe_: Subscription;
 
   constructor(
     private pokemonService: PokemonService,
-    private modalService: BsModalService) { }
+    private modalService: BsModalService
+  ) { }
+
+
+  ngOnDestroy(): void {
+    if (this.unsubscribe_ !== undefined) { this.unsubscribe_.unsubscribe(); }
+  }
 
   ngOnInit(): void {
-    this.pokemonService.getPokemon().toPromise().then(
-      (data) => {
-        this.pokemonInfo(data);
-      }
-    )
+    this.getAllPokemons();
   }
 
 
+  public getAllPokemons() {
+    this.pokemonService.getAllPokemon().toPromise().then(
+      (data) => {
+        this.pokemonInfo(data);
+      }
+    );
+  }
+
   public scrollPage() {
+    console.log('parou nada')
     this.pokemonService.getNextPagePokemon(this.nextPage).toPromise().then(
       (data) => {
         this.pokemonInfo(data);
@@ -38,6 +54,7 @@ export class HomeComponent implements OnInit {
 
   private pokemonInfo(data) {
     this.nextPage = data.next;
+    this.notFound = false;
     for (let i = 0; i < data.results.length; i++) {
       const element = data.results[i];
       const urlSplit = element.url.split('/');
@@ -50,9 +67,9 @@ export class HomeComponent implements OnInit {
     }
   }
 
-  openModal(id: number) {
+  public openModal(id: number) {
     this.modalRef = this.modalService.show(PokemonComponent, {
-      initialState: {pokemonId: id}, 
+      initialState: { pokemonId: id },
       class: 'modal-dialog-centered, modal-lg',
       keyboard: false,
       animated: true,
@@ -60,4 +77,30 @@ export class HomeComponent implements OnInit {
   }
 
 
+  public searchPokemon() {
+    this.pokemonList = [];
+
+    if (this.findPokemon === '' || this.findPokemon === undefined ) {
+      this.getAllPokemons();
+      return;
+    }
+
+    this.loading = true;
+    this.unsubscribe_ = this.pokemonService.getInfoPokemon(this.findPokemon).subscribe(
+      (data) => {
+        const model = {
+          id: data.id,
+          name: data.name
+        }
+        this.loading = false;
+        this.notFound = false;
+        this.pokemonList.push(model);
+      },
+      (err) => {
+        this.loading = false;
+        this.errorMessage = err.error;
+        this.notFound = true;
+      }
+    )
+  }
 }
